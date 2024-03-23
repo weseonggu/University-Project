1260c81a387a70874d2dfd3ca4b36c51eb247b27
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-03-23 14:07:33 +0900

    baisc project file

---
+++ src/main/java/com/hallym/project/RingRingRing/RingRingRingApplication.java
@@ -0,0 +1,13 @@
+package com.hallym.project.RingRingRing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingRingRingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingRingRingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hallym/project/RingRingRing/RingRingRingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallym.project.RingRingRing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingRingRingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
