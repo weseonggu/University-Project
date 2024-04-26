865fe877762c58904cac7d28564ed9698edbaf48
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-04-26 21:21:48 +0900

    DTO 변경 작업

---
+++ src/main/java/com/hallym/project/RingRingRing/DTO/UserDTO.java
@@ -0,0 +1,34 @@
+package com.hallym.project.RingRingRing.DTO;
+
+import java.util.Set;
+
+import com.hallym.project.RingRingRing.Entity.AuthorityEntity;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	
+	private Long id;
+	
+	@NotNull(message = "이름을 입력해주세요")
+	private String name;
+	
+	@NotNull(message = "이메일을 입력해주세요")
+	@Email(message = "이메일 형식으로 입력해 주세요")
+	private String email;
+	
+	@NotNull(message = "비번을 입력하세요")
+	@Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\\\":{}|<>])(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$", 
+	message = "8자 이상글자에 적어도 하나의 특수문자, 영문자, 숫자를 포함해야 합니다.")
+	private String pwd;
+	
+}
