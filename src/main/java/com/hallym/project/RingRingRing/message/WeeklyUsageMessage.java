f03ef93a4177bc5e972382417cb875f2df732c14
Altair5869 <taehun@gimtaehun-ui-MacBookAir.local>
2024-05-12 18:12:33 +0900

    주간 통계 기능 구현

---
+++ src/main/java/com/hallym/project/RingRingRing/message/WeeklyUsageMessage.java
@@ -0,0 +1,17 @@
+package com.hallym.project.RingRingRing.message;
+
+import com.hallym.project.RingRingRing.Entity.WeeklyUsageAnalysisEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WeeklyUsageMessage {
+    private Long duration;
+    private String message;
+}
---
+++ src/main/java/com/hallym/project/RingRingRing/weeklyUsageAnalysis/WeeklyUsageService.java
@@ -0,0 +1,55 @@
+package com.hallym.project.RingRingRing.weeklyUsageAnalysis;
+
+import com.hallym.project.RingRingRing.DTO.WeeklyUsageDTO;
+import com.hallym.project.RingRingRing.Entity.UserEntity;
+import com.hallym.project.RingRingRing.Entity.WeeklyUsageAnalysisEntity;
+import com.hallym.project.RingRingRing.message.WeeklyUsageMessage;
+import com.hallym.project.RingRingRing.repository.UserRepository;
+import com.hallym.project.RingRingRing.repository.WeeklyUsageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@EnableScheduling
+public class WeeklyUsageService {
+    private final WeeklyUsageRepository weeklyUsageRepository;
+    private final UserRepository userRepository;
+
+    public ResponseEntity<WeeklyUsageMessage> getWeeklyUsageByEmail(String email) {
+
+        UserEntity user = userRepository.findByEmail(email).get(0);
+
+        Long duration = 0L;
+        LocalDate now = LocalDate.now();
+        LocalDate startOfWeek = now.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        LocalDate endOfWeek = now.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+
+//        WeeklyUsageAnalysisEntity testUsage = WeeklyUsageAnalysisEntity.builder()
+//                        .user(user)
+//                        .timestamp(15)
+//                                .duration(duration)
+//                                        .week(LocalDateTime.now()).build();
+//        weeklyUsageRepository.save(testUsage);
+
+        List<WeeklyUsageDTO> weeklyUsageDTO = weeklyUsageRepository.findWeeklyUsageByEmailAndTimestampBetween(email, startOfWeek.atStartOfDay(), endOfWeek.atTime(LocalTime.MAX));
+        for(WeeklyUsageDTO wud : weeklyUsageDTO){
+            duration += wud.getTimestamp();
+        }
+
+        return new ResponseEntity<WeeklyUsageMessage>(new WeeklyUsageMessage(duration, "평균 연습 시간"), HttpStatus.OK);
+
+    }
+}
